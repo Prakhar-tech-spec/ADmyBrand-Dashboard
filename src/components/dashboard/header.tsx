56ddba8f58a79b1cee3e2e33dd4d5d77db7d84a6
Prakhar Creatific <prakharr.creatific@gmail.com>
2025-08-04 09:56:13 +0000

    also for mobile and tablet view for them there is only search icon so th

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -8,8 +8,7 @@ import { Input } from '@/components/ui/input';
 export function Header() {
   return (
     <header className="sticky top-0 z-10 flex h-24 items-end justify-between gap-4 bg-background/80 pb-4 backdrop-blur-sm px-4 md:px-6 lg:px-8">
-      <div className="relative flex-1 md:flex-none">
-        <div className="hidden md:block relative">
+      <div className="relative hidden flex-1 md:flex-none md:block">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
           <Input
             type="search"
@@ -19,13 +18,12 @@ export function Header() {
           <div className='absolute right-2 top-1/2 -translate-y-1/2 flex items-center gap-1 text-muted-foreground text-sm'>
               <Command className='w-4 h-4' />F
           </div>
-        </div>
-        <Button variant="outline" size="icon" className="rounded-full h-11 w-11 bg-card md:hidden">
-          <Search className="h-5 w-5" />
-          <span className="sr-only">Search</span>
-        </Button>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex flex-1 items-center justify-end gap-2">
+        <Button variant="outline" size="icon" className="rounded-full h-11 w-11 bg-card md:hidden">
+            <Search className="h-5 w-5" />
+            <span className="sr-only">Search</span>
+        </Button>
         <Button variant="outline" size="icon" className="rounded-full h-11 w-11 bg-card">
           <Bell className="h-5 w-5" />
           <span className="sr-only">Notifications</span>
